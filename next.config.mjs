fb418af3fb1be8d9c5dfc932b5819cf0648a9b96
Juan Marco Raimundo <128002048+JuanMRaimundo@users.noreply.github.com>
2025-12-27 19:00:38 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Ignora errores de ESLint durante el build para que no detengan el despliegue
+  
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // Ignora errores de TypeScript durante el build (útil para deploys rápidos)
+  
   typescript: {
     ignoreBuildErrors: true,
   },
